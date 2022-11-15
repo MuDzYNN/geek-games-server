9a6199cb1e800a1b1b6c4ec51cba36f514e7d6d7
MuDzYNN <MudzynDev@gmail.com>
2022-11-15 23:44:30 +0100

    xd

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const app = require('express')();
 const Logger = require('./libs/logger');
 require('dotenv').config();
 
+app.get('/', (req, res) => {
+    res.send('Hello!');
+});
+
 app.use('/', require('./api'));
 
 app.listen(process.env.APP_PORT, () => Logger('warn', `Listening at http://localhost:${process.env.APP_PORT}`));
